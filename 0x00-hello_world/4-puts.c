dd78e6b62d1a380a42b92d32311d3668cf128ac0
MARKTMM <markmwangi304@gmail.com>
2023-03-16 18:22:26 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  *  main - this is the main function
- *  Return: always 0
+ *  Return: 0 (Success)
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
