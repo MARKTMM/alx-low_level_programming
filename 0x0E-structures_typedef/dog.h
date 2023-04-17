4cf29cad2fc48ba9698954e7b6947ea6cd698f9c
MARKTMM <markmwangi304@gmail.com>
2023-04-17 15:56:12 -0700

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef MARKTMM
+#define MARKTMM
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Member two
+ * @owner: Member Three
+ *
+ * Description:-l description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
