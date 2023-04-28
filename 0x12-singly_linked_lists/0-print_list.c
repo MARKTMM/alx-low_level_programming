f17791679ce0d37ddf3b21865652269a546eea7f
MARKTMM <markmwangi304@gmail.com>
2023-04-27 19:47:59 -0700

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,23 +9,22 @@
  */
 size_t print_list(const list_t *head)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (head != NULL)
-    {
-        if (head->str == NULL)
-        {
-            printf("[0] (nil)\n");
-        }
-        else
-        {
-            printf("[%u] %s\n", head->len, head->str);
-        }
+	while (head != NULL)
+	{
+		if (head->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", head->len, head->str);
+		}
 
-        head = head->next;
-        count++;
-    }
+		head = head->next;
+		count++;
+	}
 
-    return count;
+	return (count);
 }
-
