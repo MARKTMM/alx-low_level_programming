4b59e45046ec4af1e246bde51d593098eaabe017
MARKTMM <markmwangi304@gmail.com>
2023-04-27 18:36:45 -0700

    returns the number of elements in a linked list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,13 +9,13 @@
  */
 size_t list_len(const list_t *head)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (head != NULL)
-    {
-        count++;
-        head = head->next;
-    }
+	while (head != NULL)
+	{
+		count++;
+		head = head->next;
+	}
 
-    return count;
+	return (count);
 }
