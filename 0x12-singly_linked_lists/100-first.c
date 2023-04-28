c9e0ea348bf71ff439b66a34dc4fe179622332be
MARKTMM <markmwangi304@gmail.com>
2023-04-27 19:43:14 -0700

    Mark

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,30 +1,13 @@
 #include <stdio.h>
 
 void first(void) __attribute__ ((constructor));
-void main_function(void);
 
 /**
- * first - prints a sentence before the main
- * function is executed
+ * first - print a sentence before the main
+ * function is execut
  */
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
-}
-
-/**
- * main_function - the entry point of the program
- *
- * Return: Always 0
- */
-void main_function(void)
-{
-	printf("I have just begun to run...\n");
-}
-
-int main(void)
-{
-	main_function();
-	return (0);
 }
