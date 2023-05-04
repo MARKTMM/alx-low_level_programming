e85243dd9965e6b68a054d1dbef0c012532bdfb5
MARKTMM <markmwangi304@gmail.com>
2023-05-04 13:21:58 -0700

    correct

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,20 +10,16 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	if (!b)
-	{
-		return (0);
-	}
-
+	int i;
 	unsigned int dec_val = 0;
 
-	for (int i = 0; b[i]; i++)
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
-		{
 			return (0);
-		}
-
 		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
