9048797dea8979759920ab82761f2b0d88eb2d79
MARKTMM <markmwangi304@gmail.com>
2023-05-04 13:19:15 -0700

    correction

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,9 +4,9 @@
  * binary_to_uint - takes a string of binary digits as input and returns
  * an unsigned integer representing the decimal equivalent of the binary num.
  *
- * @param b: A pointer to a string of binary digits. It must not be null.
+ * @b: A pointer to a string of binary digits. It must not be null.
  *
- * @return: character other than '0' or '1',the function returns zero.
+ * Return: character other than '0' or '1',the function returns zero.
  */
 unsigned int binary_to_uint(const char *b)
 {
